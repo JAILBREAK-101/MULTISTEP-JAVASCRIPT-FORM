199b4a8f0a3af43fc49200a6fba6ba414626ccee
JAIL-BREAK <legendn351540@gmail.com>
2023-02-18 02:22:57 +0100

    Committed First File

---
+++ multi-step-form-main/TypeScript/Script.js
@@ -0,0 +1,10 @@
+// JS Code for the Logic
+// THE THINGS NEEDED FOR THE JS OF THIS PROJECT.
+
+// THE FORM VALIDATION FOR THE FORM.
+const errorMessages = document.querySelectorAll(".error-message");
+
+// HIDE ALL THE ERROR MESSAGES.
+for (let error of errorMessages) {
+  error.classList.add("display-none");
+}
